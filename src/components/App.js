969b3f4db8650a8e3a1bc2c73badcee638f1804c
ratalay <rahmiiatalay@gmail.com>
2021-04-25 22:44:01 +0100

    LocationDetails Rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import "../styles/App.css";
 import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails
+        // eslint-disable-next-line react/destructuring-assignment
+        city={props.location.city}
+        // eslint-disable-next-line react/destructuring-assignment
+        country={props.location.country}
+      />
     </div>
   );
-}
+};
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
