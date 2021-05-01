da8aa509152e0a252a6bb40143eee5c537e9190a
ratalay <rahmiiatalay@gmail.com>
2021-05-01 22:33:43 +0100

    New props need validation in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import "../styles/App.css";
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
 const App = (props) => {
+  const { location } = props;
   return (
     <div className="App">
       <LocationDetails
         // eslint-disable-next-line react/destructuring-assignment
-        city={props.location.city}
+        city={location.city}
         // eslint-disable-next-line react/destructuring-assignment
-        country={props.location.country}
+        country={location.country}
       />
+      <ForecastSummaries />
     </div>
   );
 };
---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ForecastSummaries = () => <div className="forecast-summaries" />;
+
+export default ForecastSummaries;
